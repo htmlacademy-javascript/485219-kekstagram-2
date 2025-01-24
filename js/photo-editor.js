a763a81ee49a413b61968c94de791ed0c18287c4
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2025-01-24 22:26:30 +0300

    синхронизация между ПК

--- js/photo-editor.js
+++ js/photo-editor.js
@@ -4,6 +4,49 @@ const editPhotoFormElement = formElement.querySelector('.img-upload__overlay');
 const closeButtonElement = formElement.querySelector('.img-upload__cancel');
 const bodyElement = document.body;
 
+const pristine = new Pristine(formElement);
+
+function validateHashtag(value) {
+  if (!value.trim()) {
+    return true;
+  }
+
+  const hashtags = value.trim().split(/\s+/);
+  const hashtagPattern = /^#[a-zа-яё0-9]{1,19}$/i;
+
+  if (hashtags.length > 5) {
+    return false;
+  }
+
+  hashtags.forEach((hashtag) => {
+    if (!hashtagPattern.test(hashtag)) {
+      return false;
+    }
+  });
+
+  const lowerCaseHashtags = hashtags.map((tag) => tag.toLowerCase());
+  const uniqueHashtags = new Set(lowerCaseHashtags);
+  if (uniqueHashtags.size !== hashtags.length) {
+    return false;
+  }
+
+  return true;
+}
+
+pristine.addValidator(formElement.querySelector('.text__hashtags'), validateHashtag);
+
+formElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
+
 uploadInputElement.addEventListener('change', () => {
   editPhotoFormElement.classList.remove('hidden');
   editPhotoFormElement.setAttribute('tabindex', 0);
